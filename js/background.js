844265135f088fe5e9cff4972b559bff506c1896
yamamaki <dingzhm93@gmail.com>
2014-12-18 11:05:55 +0800

    enhance

--- js/background.js
+++ js/background.js
@@ -1,24 +1,25 @@
 
 chrome.runtime.onConnect.addListener(function (port) {
     port.onMessage.addListener(function (msg) {
-        if (msg == "connection request") {
-            port.postMessage("connection established");
-        }
-        else if (msg == "result received") {
+        if (msg == "retJson received") {
             port.disconnect();
         }
-        else {
+        else if (msg.X && msg.Y) {
+            posX = msg.X;
+            posY = msg.Y;
+        }
+        else if (msg.text) {
             $.ajax({
-                url: "http://fanyi.youdao.com/openapi.do?keyfrom=capric0rn&key=1610565582&type=data&doctype=json&version=1.1&q=" + msg,
+                url: "http://fanyi.youdao.com/openapi.do?keyfrom=capric0rn&key=1610565582&type=data&doctype=json&version=1.1&q=" + msg.text,
                 type: 'GET',
                 dataType: 'json',
                 async: false,
                 success: function(retJson) {
                     if (retJson.basic) {
-                        port.postMessage(retJson.basic.explains);
+                        port.postMessage({retJson: retJson.basic.explains});
                     }
                     else {
-                        port.postMessage(retJson.translation);
+                        port.postMessage({retJson: retJson.translation});
                     }
                 }
             })
--- js/simDict.js
+++ js/simDict.js
@@ -1,50 +1,81 @@
+var posX = -1, posY = -1;
 
-$(document).ready(function () {
-//    $("head").append("<link>");
-//    css = $("head").children(":last");
-//    css.attr({
-//        rel: "stylesheet",
-//        type: "text/css",
-//        href: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"
-//    });
-    $(document).mouseup(function (e) {
-        var tag = e.target.tagName.toLowerCase();
-        var item = fetchText();
-        if (item) {
-            var ele = $(e.target);
-            var tmp = ele.html().split(item);
-            var port = chrome.runtime.connect();
-            port.postMessage("connection request");
-            port.onMessage.addListener(function (msg) {
-                if (msg == "connection established") {
-                    port.postMessage(item);
-                }
-                else {
-                    var content = tmp[0];
-                    for (var i=1; i<tmp.length; ++i) {
-                        var ret = JSON.stringify(msg).replace(/\"/g,"'");
-                        content += "<a href=# title=\""+ret+"\">"+item+"</a>"+tmp[i]
-                    }
-                    ele.html(content);
-                    port.postMessage("result received");
-                }
-            });
-        }
-    });
-});
+function divShow(e, cls, ret, dir) {
+    divHide(cls);
+    $('<div class=\"'+cls+'\"></div>').appendTo('body');
+    $('div.'+cls).html(ret);
+    setCSS(cls);
+    setPos(e, cls, dir);
+}
+
+function setPos(e, cls, dir) {
+    var X = -1, Y = -1;
+    switch (dir) {
+        case "top":
+            X = e.pageX - 4;
+            Y = e.pageY - 4;
+            break;
+        case "bottom":
+            X = e.pageX + 4;
+            Y = e.pageY + 4;
+            break;
+    }
+    $('div.'+cls).css({left: X, top: Y});
+}
+
+function divHide(cls) {
+    $('div.'+cls).remove();
+}
+
+function setCSS(cls) {
+    $('div.'+cls).css({
+        "background-color": "#666",
+        "color": "white",
+        "margin": "8px",
+        "padding": "8px",
+        "z-index": "100",
+        "position": "absolute",
+        "opacity": "2",
+        "border-radius": "8px"
+    })
+}
 
 function fetchText() {
-    var text;
+    var text, parentNode;
     if (document.selection) {
+        parentNode = document.selection.createRange().parentElement();
         text = document.selection.createRange().text;
     }
     else if (window.getSelection) {
+        var selection = window.getSelection();
+        if (selection.rangeCount > 0) {
+            parentNode = selection.getRangeAt(0).startContainer.parentNode;
+        }
         text = window.getSelection().toString();
-    }
-    else if (document.getSelection) {
-        text = document.getSelection().toString();
     }
     return text;
 }
 
-
+$(document).ready(function () {
+    $(document).mouseup(function (e) {
+        var retJson, text;
+        text = fetchText();
+        if (text) {
+            var port = chrome.runtime.connect();
+            port.postMessage({"text": text});
+            port.onMessage.addListener(function (msg) {
+                if (msg.retJson) {
+                    retJson = msg.retJson;
+                    port.postMessage("retJson received");
+                }
+            });
+            $(e.target).hover(function() {
+                if (text === fetchText() && retJson) {
+                    divShow(e, "sd-tooltips", JSON.stringify(retJson).replace(/"/g, "'"), "bottom");
+                }
+            }, function() {
+                divHide("sd-tooltips");
+            });
+        }
+    });
+});
